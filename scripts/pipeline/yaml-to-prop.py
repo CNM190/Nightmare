ea04282ef4a75ed26e1dda9b65370c4abbef7390
Michael Driscoll <mbdriscoll@gmail.com>
2014-12-11 20:05:08 -0800

    debugging yaml-to-prop

--- scripts/pipeline/yaml-to-prop.py
+++ scripts/pipeline/yaml-to-prop.py
@@ -2,7 +2,7 @@ import yaml, sys, os
 
 yamlfile = sys.argv[1]
 
-requested_shots = os.environ["SHOTS"].split(',')
+requested_shots = os.environ["SHOTS"][1:-1].split(',')
 print "Requested shots: %s" % requested_shots
 
 with open(yamlfile) as yamltext:
@@ -14,8 +14,8 @@ for shot in shots:
         safe_name = name.replace(' ', '_')
         print "writing properties file for %s" % safe_name
         with open("%s.properties" % safe_name, 'w') as props:
-            print >>props, "SHOT_NAME=%s"       % shot['name']
-            print >>props, "MAYAFILE=scenes/%s" % shot['file']
-            print >>props, "CAMERA=%s"          % shot['camera']
-            print >>props, "START_FRAME=%s"     % shot['startFrame']
-            print >>props, "END_FRAME=%s"       % shot['endFrame']
+            print >>props, "SHOT_NAME=%s"        % shot['name']
+            print >>props, "MAYA_FILE=scenes/%s" % shot['file']
+            print >>props, "CAMERA=%s"           % shot['camera']
+            print >>props, "START_FRAME=%s"      % shot['startFrame']
+            print >>props, "END_FRAME=%s"        % shot['endFrame']
