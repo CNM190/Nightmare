79be92caee36405bddd54f0687ccc8ff8db46df9
Michael Driscoll <mbdriscoll@gmail.com>
2014-12-11 17:48:53 -0800

    updates to metadata script

--- scripts/pipeline/overlay.py
+++ scripts/pipeline/overlay.py
@@ -22,6 +22,7 @@ class AnimatedTextClip(VideoClip):
         return np.array(frame)
 
 def main():
+    OVERLAY     = os.environ["OVERLAY_METADATA"] != "false"
     filename    = os.environ["MAYA_FILE"]
     camera      = os.environ["CAMERA"]
     renderer    = os.environ["RENDERER"]
@@ -36,25 +37,27 @@ def main():
     images = glob.glob("./FRAME=*/frame.png")
     clip = ImageSequenceClip(images, fps=24)
 
-    d = clip.duration
-    nFrames = clip.duration * 24
-    gitrev = os.popen("git rev-parse --short HEAD").read(12)[:-1]
-    gitjenkins = "git %s, jenkins %d" % (gitrev, build_number)
-    author = os.popen('git log -n 1 --pretty=format:%%an "%s"' % filename).read()
-    rcam = "%s: %s" % (renderer, camera)
+    if OVERLAY:
+        d = clip.duration
+        nFrames = clip.duration * 24
+        gitrev = os.popen("git rev-parse --short HEAD").read(12)[:-1]
+        gitjenkins = "git %s, jenkins %d" % (gitrev, build_number)
+        author = os.popen('git log -n 1 --pretty=format:%%an "%s"' % filename).read()
+        rcam = "%s: %s" % (renderer, camera)
 
-    def get_overlay_text(t):
-        frame_in_shot = int((t+0.5/24.0)*24+1)
-        frame_in_file = start_frame + frame_in_shot
-        frame = "frame %3d/%d (shot), %3d/%d (file)" % (frame_in_shot, nFrames, frame_in_file, end_frame)
-        header = "%s %s %s" % (author.ljust(42)[:42], gitjenkins.center(41)[:41], date_now.rjust(42)[:42])
-        footer = "%s %s %s" % (rcam.ljust(42)[:42],   filename.center(41)[:41],   frame.rjust(42)[:42])
-        return header, footer
+        def get_overlay_text(t):
+            frame_in_shot = int((t+0.5/24.0)*24+1)
+            frame_in_file = start_frame + frame_in_shot
+            frame = "frame %3d/%d (shot), %3d/%d (file)" % (frame_in_shot, nFrames, frame_in_file, end_frame)
+            header = "%s %s %s" % (author.ljust(42)[:42], gitjenkins.center(41)[:41], date_now.rjust(42)[:42])
+            footer = "%s %s %s" % (rcam.ljust(42)[:42],   filename.center(41)[:41],   frame.rjust(42)[:42])
+            return header, footer
 
-    overlay_frame = AnimatedTextClip(get_overlay_text, size=(x_size,y_size+90))
-    clips = [overlay_frame, clip.set_position((0, 45))]
-    clip = CompositeVideoClip(clips, size=(x_size,y_size+90)).set_duration(d)
-    clip.write_videofile("overlay.mp4", fps=24, audio=False)
+        overlay_frame = AnimatedTextClip(get_overlay_text, size=(x_size,y_size+90))
+        clips = [overlay_frame, clip.set_position((0, 45))]
+        clip = CompositeVideoClip(clips, size=(x_size,y_size+90)).set_duration(d)
+
+    clip.write_videofile("scene.mp4", fps=24, audio=False)
 
 if __name__ == "__main__":
     main()
