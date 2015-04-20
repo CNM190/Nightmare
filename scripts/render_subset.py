33bc38f6b0169c1ab07504ef60be9294b5874513
Michael Driscoll <mbdriscoll@gmail.com>
2015-04-20 16:20:13 -0700

    support explicit job keys

--- scripts/render_subset.py
+++ scripts/render_subset.py
@@ -1,5 +1,12 @@
-import yaml, datetime
+import yaml, datetime, sys
 import maya_cli
+
+if len(sys.argv) == 2:
+    job = sys.argv[1]
+    print "Using explicit job id:", job
+else:
+    job = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
+    print "Using auto job id:", job
 
 with open('scenes [RENDER]/shots.yaml') as s:
     shots = yaml.load(s)
@@ -8,11 +15,10 @@ for i, shot in enumerate(shots):
     name = shot['name']
     frameN, frame0 = shot['endFrame'], shot['startFrame']
     camera = shot['camera']
-    print "% 2d: %s %d:%d %s" % (i, name, frame0, frameN, camera)
+    print "%2d: %s %d:%d %s" % (i, name, frame0, frameN, camera)
 
 shot_ids = [int(i) for i in raw_input("Enter the number(s) of the shot to render, separated by spaces: ").split(' ')]
 
-job = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
 for i in shot_ids:
     maya_cli.render(shots[i], job_key=job)
 
