43f84c07015e6b177c2319327d89805249633ac9
Michael Driscoll <mbdriscoll@gmail.com>
2015-04-21 17:58:52 -0700

    ui tweak in rendering script

--- scripts/maya_cli.py
+++ scripts/maya_cli.py
@@ -1,14 +1,6 @@
 import os, subprocess, pipes, shutil, datetime
 
-def render(shot, frame=None, tractor=True):
-    name = shot['name']
-    camera = shot['camera']
-    scene = os.path.join(os.getcwd(), "scenes [RENDER]", shot['file'])
-    if frame is None:
-        frame0, frameN = shot['startFrame'], shot['endFrame']
-    else:
-        frame0, frameN = frame, frame
-
+def render(shots, frame=None, tractor=True):
     job_key = str(datetime.datetime.now().strftime("%Y%m%d%H%M%S"))
     job_key = raw_input("Enter job key (%s): " % job_key) or job_key
     rm_rman_dir = (raw_input("Zap renderman directory (Y/n): ") or 'y') in ('Y', 'y', 'yes', 'Yes')
@@ -22,30 +14,39 @@ def render(shot, frame=None, tractor=True):
         if confirmed:
             shutil.rmtree(renderman_dir)
 
-    image_name = "%s.%s" % (name.replace(' ', '_'), camera)
-    cmd = [
-        "/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render",
-        "-r", "rman",
-        "-proj", str(os.getcwd()),
-        "-res", "480", "270",
-        "-cam", camera,
-        "-of", "OpenEXR",
-        "-im", image_name,
-        "-rd", output_dir,
-        "-pad", "3",
-        "-s", str(frame0),
-        "-e", str(frameN),
-        "-fnc", "name.#.ext",
-    ]
-
-    if tractor:
-        cmd += ["-spool", 'immediate rib, remote render']
-
-    cmd += [scene]
-
-    cmd_text =  " ".join([pipes.quote(token) for token in cmd])
-    print "calling: ", cmd_text
-    #subprocess.check_call(cmd_text, shell=True)
+    for shot in shots:
+        name = shot['name']
+        camera = shot['camera']
+        scene = os.path.join(os.getcwd(), "scenes [RENDER]", shot['file'])
+        if frame is None:
+            frame0, frameN = shot['startFrame'], shot['endFrame']
+        else:
+            frame0, frameN = frame, frame
+    
+        image_name = "%s.%s" % (name.replace(' ', '_'), camera)
+        cmd = [
+            "/Applications/Autodesk/maya2015/Maya.app/Contents/bin/Render",
+            "-r", "rman",
+            "-proj", str(os.getcwd()),
+            "-res", "480", "270",
+            "-cam", camera,
+            "-of", "OpenEXR",
+            "-im", image_name,
+            "-rd", output_dir,
+            "-pad", "3",
+            "-s", str(frame0),
+            "-e", str(frameN),
+            "-fnc", "name.#.ext",
+        ]
+    
+        if tractor:
+            cmd += ["-spool", 'immediate rib, remote render']
+    
+        cmd += [scene]
+    
+        cmd_text =  " ".join([pipes.quote(token) for token in cmd])
+        print "calling: ", cmd_text
+        subprocess.check_call(cmd_text, shell=True)
 
     print "Render job submitted with id:", "autorender_%s" % job_key
     print "Copy locally with:", "rsync -avz shay:%s ~/Desktop/" % output_dir
